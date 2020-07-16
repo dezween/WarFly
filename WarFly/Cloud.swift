f31a256bef7a8abaecda6a29e2a5745f8a7df099
Вячеслав Титов <vcslvttv@gmail.com>
2020-07-16 19:41:26 +0500

    Player plane class

--- WarFly/Cloud.swift
+++ WarFly/Cloud.swift
@@ -22,7 +22,7 @@ final class Cloud: SKSpriteNode, GameBackgroundSpriteable {
         cloud.setScale(randomScaleFactor)
         cloud.position = point
         cloud.zPosition = 10
-        
+        cloud.run(move(from: point))
         return cloud
     }
     
@@ -36,9 +36,26 @@ final class Cloud: SKSpriteNode, GameBackgroundSpriteable {
     }
     
     fileprivate static var randomScaleFactor: CGFloat {
+        
         let distribution = GKRandomDistribution(lowestValue: 20, highestValue: 30)
         let randomNumber = CGFloat(distribution.nextInt()) / 10
         
         return randomNumber
     }
+    
+    fileprivate static func move(from point: CGPoint) -> SKAction {
+        
+        let movePoint = CGPoint(x: point.x, y: -200)
+        let moveDistance = point.y + 200
+        let movementSpeed: CGFloat = 15.0
+        let duration = moveDistance / movementSpeed
+        return SKAction.move(to: movePoint, duration: TimeInterval(duration))
+    }
 }
+
+
+
+
+
+
+
