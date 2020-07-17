e6a114609690fdfc054f264694d40accc298fcfd
Вячеслав Титов <vcslvttv@gmail.com>
2020-07-17 20:16:37 +0500

    Cleaning our project

--- WarFly/GameScene.swift
+++ WarFly/GameScene.swift
@@ -8,19 +8,17 @@
 
 import SpriteKit
 import GameplayKit
-import CoreMotion
 
 class GameScene: SKScene {
     
-    let motionManager = CMMotionManager()
-    var xAcceleration: CGFloat = 0
-    var player: SKSpriteNode!
+    var player: PlayerPlane!
     
     override func didMove(to view: SKView) {
         
         configureStartScene()
         spawnClouds()
         spawnIslands()
+        player.performFly()
     }
     
     fileprivate func spawnClouds() {
@@ -55,38 +53,21 @@ class GameScene: SKScene {
         
         let screen = UIScreen.main.bounds
         
-        
         let island1 =  Island.populate(at: CGPoint(x: 100, y: 200))
         self.addChild(island1)
         
         let island2 =  Island.populate(at: CGPoint(x: self.size.width - 100, y: self.size.height - 200))
         self.addChild(island2)
         
-
-        
         player = PlayerPlane.populate(at: CGPoint(x: screen.size.width / 2, y: 100))
         self.addChild(player)
         
-        motionManager.accelerometerUpdateInterval = 0.2
-        motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (data, error) in
-            if let data = data {
-                let acceleration = data.acceleration
-                self.xAcceleration = CGFloat(acceleration.x) * 0.7 + self.xAcceleration * 0.3
-            }
-        }
+
     }
     
     override func didSimulatePhysics() {
-        super.didSimulatePhysics()
         
-        player.position.x += xAcceleration * 50
-        
-        if player.position.x < -70 {
-            player.position.x = self.size.width + 70
-        } else if player.position.x > self.size.width + 70 {
-            player.position.x = -70
-        }
-        
+        player.checkPosition()
         enumerateChildNodes(withName: "backgroundSprite") { (node, stop) in
             if node.position.y < -199 {
                 node.removeFromParent()
